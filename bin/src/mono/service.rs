62c4dc3947ecc6c2419947a672ab0c890625b7be
maxabcr2000 <maxabcr2000@gmail.com>
2022-04-06 16:29:43 +0800

    Implement Logging and Response encoding

--- bin/src/mono/service.rs
+++ bin/src/mono/service.rs
@@ -1,6 +1,11 @@
+extern crate hex;
 use actix_cors::Cors;
-use actix_web::{error, get, http::header, post, web, App, Error, HttpResponse, HttpServer};
+use actix_web::{
+    error, get, http::header, middleware::Logger, post, web, App, Error, HttpRequest, HttpResponse,
+    HttpServer,
+};
 use chrono::Utc;
+use env_logger;
 use log;
 use reqwest::Client;
 use serde::{Deserialize, Serialize};
@@ -10,8 +15,10 @@ pub async fn build_http_service(
     dapp_port: String,
     http_dispatcher_url: String,
 ) -> std::io::Result<()> {
+    env_logger::init_from_env(env_logger::Env::new().default_filter_or("debug"));
     let addr = SocketAddr::from(([127, 0, 0, 1], dapp_port.parse::<u16>().unwrap()));
 
+    log::debug!("build_http_service");
     HttpServer::new(move || {
         App::new()
             .app_data(http_dispatcher_url.clone())
@@ -26,6 +33,7 @@ pub async fn build_http_service(
                     ])
                     .max_age(3600),
             )
+            .wrap(Logger::new("%a %{User-Agent}i"))
             .service(healthz)
             .service(inspect)
             .service(advance_state)
@@ -69,21 +77,41 @@ pub struct AdvanceStateRequest {
     operation: String,
 }
 
+#[derive(Debug, Clone, Deserialize, Serialize)]
+pub struct AdvanceStateResponse {
+    result: String,
+}
+
 #[post("/advance")]
 async fn advance_state(
-    http_dispatcher_url: String,
-    req: web::Json<AdvanceStateRequest>,
+    json_req: web::Json<AdvanceStateRequest>,
+    http_req: HttpRequest,
 ) -> Result<HttpResponse, Error> {
     log::debug!("advance_state");
 
     //#TODO: Match: JoinGame / Move
 
     let client = Client::new();
+    let http_dispatcher_url =
+        http_req
+            .app_data::<String>()
+            .ok_or(error::ErrorInternalServerError(
+                "http_dispatcher_url data not found",
+            ))?;
 
     log::debug!("Call to Http Dispatcher: Adding Notice");
     let mut resp_payload = HashMap::new();
-    //#TODO: replace "accept" with result hex
-    resp_payload.insert("payload", "accept");
+
+    let response = AdvanceStateResponse {
+        result: String::from("Test"),
+    };
+
+    let serialized = serde_json::to_string(&response).unwrap();
+    log::debug!("Serialized response: {}", serialized);
+    let hex_response = hex::encode(serialized);
+    log::debug!("Hex-encoded response: {}", hex_response);
+
+    resp_payload.insert("payload", hex_response);
 
     let notice_resp = client
         .post(format!("{}/notice", http_dispatcher_url))
@@ -117,5 +145,5 @@ async fn advance_state(
 
     log::debug!("Received finish status {}", finish_resp.status());
 
-    Ok(HttpResponse::Ok().json(req))
+    Ok(HttpResponse::Ok().json(json_req))
 }
